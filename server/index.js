d4c3e4242697acdb18e330407c6b4b4c335a029b
Matic <slomatic@gmail.com>
2019-05-18 21:19:33 +0200

    redo

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const path = require("path");
 const port = 3001;
 const app = express();
 
-app.use(express.static(path.join(__dirname, "client/build")))
+app.use(express.static(path.join(__dirname, "../client/build")))
 const dbRoute = "mongodb://localhost:27017/blog"
 mongoose.connect(dbRoute, { useNewUrlParser: true })
 
