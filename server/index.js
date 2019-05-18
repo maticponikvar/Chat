c562a18d7d66e91b11e5e9d26fd5dea2538a85de
Matic <slomatic@gmail.com>
2019-05-18 14:07:49 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -36,9 +36,9 @@ mongoose.set('useCreateIndex', true);
 app.use(function (req, res, next) {
   // console.log("CORS")
   // res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000")
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+  // res.header('Access-Control-Allow-Credentials', true);
+  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
